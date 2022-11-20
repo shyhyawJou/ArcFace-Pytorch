4c46c2379b1959bb5aeea2d31946bd25447c5ad6
Shi-Yao, ZHOU <fish78991@gmail.com>
2022-11-20 15:59:51 +0800

    Update loss.py

--- loss.py
+++ loss.py
@@ -1,30 +1,34 @@
 import torch
 import torch.nn as nn
 from torch.nn import functional as F
-from torch.linalg import norm as torch_norm
+from torch import linalg
+
 
 
 
 class ArcFace(nn.Module):
-    def __init__(self, cin, cout, s=32, m=0.5):
+    def __init__(self, cin, cout, s=None, m=0.5):
         super().__init__()
-        self.m = m
         self.s = s
+        self.sin_m = torch.sin(torch.tensor(m))
+        self.cos_m = torch.cos(torch.tensor(m))
         self.cout = cout
-        self.fc = nn.Linear(cin, cout, bias=False)                              
+        self.fc = nn.Linear(cin, cout, bias=False)
 
     def forward(self, x, label=None):
-        if label is None:
-            w_L2 = torch_norm(self.fc.weight.detach(), dim=1, keepdim=True)
-            x_L2 = torch_norm(x, dim=1, keepdim=True)
-            logit = F.linear(x / x_L2, self.fc.weight / w_L2)
-        else:
-            one_hot = F.one_hot(label, num_classes=self.cout)
-            w_L2 = torch_norm(self.fc.weight.detach(), dim=1, keepdim=True)
-            x_L2 = torch_norm(x, dim=1, keepdim=True)
-            cos = F.linear(x / x_L2, self.fc.weight / w_L2)
-            theta_yi = torch.acos(cos * one_hot)
-            logit = torch.cos(theta_yi + self.m) * one_hot + cos * (1 - one_hot)
-            logit = logit * self.s
+        w_L2 = linalg.norm(self.fc.weight.detach(), dim=1, keepdim=True).T
+        x_L2 = linalg.norm(x, dim=1, keepdim=True)
+        cos = self.fc(x)
 
-        return logit
+        if label is not None:
+            sin_m, cos_m = self.sin_m, self.cos_m
+            one_hot = F.one_hot(label, num_classes=self.cout)
+            cos = cos / (x_L2 * w_L2)
+            sin = (1 - cos ** 2) ** 0.5
+            angle_sum = cos * cos_m - sin * sin_m
+            cos = angle_sum * one_hot + cos * (1 - one_hot)
+            cos = cos * (x_L2 * w_L2 if self.s is None else self.s)
+                        
+        return cos
+
+
